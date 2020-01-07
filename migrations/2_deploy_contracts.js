368265bb24b4bcdd6062c46d574100b2d7acdf11
RyoungHo <chl4745@naver.com>
2020-01-07 15:23:00 +0900

    fix. deploy uniswap

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,36 +1,55 @@
-const nftToken = artifacts.require('./token/MineralNFT.sol');
-const mineralToken = artifacts.require('./token/Mineral.sol');
-const nftMarket = artifacts.require('./MineralNFTMarket.sol');
-const fs = require('fs');
+const nftToken = artifacts.require("./token/MineralNFT.sol");
+const mineralToken = artifacts.require("./token/Mineral.sol");
+const nftMarket = artifacts.require("./MineralNFTMarket.sol");
+const uniFactory = artifacts.require("./uniswap/Factory.sol");
+const fs = require("fs");
 
 module.exports = function(deployer) {
-  deployer.deploy(nftToken, "MineralNFT", "FSI").then(() => {
-    saveABI(nftToken);
-    return deployer.deploy(mineralToken);
-  }).then(() => {
-    saveABI(mineralToken);
-    return deployer.deploy(nftMarket, nftToken.address, mineralToken.address);
-  }).then(() => {
-    saveABI(nftMarket);
-  });
+	deployer
+		.deploy(nftToken, "MineralNFT", "FSI")
+		.then(() => {
+			saveABI(nftToken);
+			return deployer.deploy(mineralToken);
+		})
+		.then(() => {
+			saveABI(mineralToken);
+			return deployer.deploy(
+				nftMarket,
+				nftToken.address,
+				mineralToken.address
+			);
+		})
+		.then(() => {
+			saveABI(nftMarket);
+			return deployer.deploy(uniFactory);
+		})
+		.then(() => {
+			saveABI(uniFactory);
+		});
 };
 
 function saveABI(contract) {
-  if (contract._json) {
-    fs.writeFile(
-      'deployed/' + contract._json.contractName + '_deployedABI',
-      JSON.stringify(contract._json.abi, 2),
-      (err) => {
-        if (err) throw err
-        console.log(`The abi of ${contract._json.contractName} is recorded on deployedABI file`)
-      });
-  }
+	if (contract._json) {
+		fs.writeFile(
+			"deployed/" + contract._json.contractName + "_deployedABI",
+			JSON.stringify(contract._json.abi, 2),
+			err => {
+				if (err) throw err;
+				console.log(
+					`The abi of ${contract._json.contractName} is recorded on deployedABI file`
+				);
+			}
+		);
+	}
 
-  fs.writeFile(
-    'deployed/' + contract._json.contractName + '_deployedAddress',
-    contract.address,
-    (err) => {
-      if (err) throw err
-      console.log(`The deployed contract address * ${contract.address} * is recorded on deployedAddress file`)
-  })
+	fs.writeFile(
+		"deployed/" + contract._json.contractName + "_deployedAddress",
+		contract.address.toLowerCase(),
+		err => {
+			if (err) throw err;
+			console.log(
+				`The deployed contract address * ${contract.address} * is recorded on deployedAddress file`
+			);
+		}
+	);
 }
